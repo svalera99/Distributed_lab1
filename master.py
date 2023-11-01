1041b9f55c9af18583849b33b84aa169e0232fa6
valery.savoskin <Valery.Savoskin@toshibagcs.com>
2023-11-01 21:26:46 +0100

    adding lab2 without deduplication yet and random compose file

--- master.py
+++ master.py
@@ -6,6 +6,7 @@ from flask import Flask, jsonify
 from flask import request
 import socketio
 from socketio.exceptions import TimeoutError
+import asyncio
 
 from args import parse_args
 
@@ -20,38 +21,56 @@ msg_lst = []
 def get_lst():
     return jsonify([f"Message number - {msg_inx}, message - {msg}" for msg_inx, msg in enumerate(msg_lst)])
 
+
+async def communicate_with_slave(
+    slave_ip,
+    slave_port,
+    data
+):
+    async with socketio.AsyncSimpleClient() as sio:
+        await sio.connect("http://" + slave_ip + ":" + slave_port)
+        await sio.emit('append_msg', data)
+
+        sent_time = time()
+        try:
+            event = await sio.receive()
+            if event != ["appended"]:
+                raise ValueError
+        except TimeoutError:
+            logger.error(f'Timed out waiting for ACK from slave on port {slave_port}')
+        except ValueError:
+            logger.error(f"Wrong ACK received from slave on port {slave_port}; recieved - {event}")
+        else:
+            duration = time() - sent_time
+            logger.debug(f'Wrote data {data} to slave ip with port {slave_port}, duration of the coroutine - {duration}')
+
+
 @app.route("/", methods=['POST'])
 async def put_lst():
     ts = time()
     concerns = request.get_json().get("w", None)
     if concerns is None:
         raise ValueError("Number of slave replicas is not provided")
+    concerns = int(concerns) - 1
 
     data = request.get_json().get("msg", "")
     msg_lst.append(data)
-    accepted_acks = 1
-    for slave_ip, slave_port in zip(cfg["slaves_ips"], cfg["slaves_port"]):
-        async with socketio.AsyncSimpleClient(logger=True, engineio_logger=True) as sio:
-            await sio.connect("http://" + slave_ip + ":" + slave_port)
-            await sio.emit('append_msg', data)
+    tasks = [
+        asyncio.create_task(communicate_with_slave(slave_ip, slave_port, data))
+        for slave_ip, slave_port in zip(cfg["slaves_ips"], cfg["slaves_port"])
+    ]
+    if concerns > 0:
+        while True:
+            done, _ = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+            tasks = [task for task in tasks if not task.done()]
 
-            if accepted_acks < concerns:
-                sent_time = time()
-                try:
-                    event = await sio.receive(timeout=cfg["sleep_duration_sec"] + 1)
-                    if event != ["appended"]:
-                        raise ValueError
-                    else:
-                        accepted_acks += 1
-                except TimeoutError:
-                    logger.error(f'Timed out waiting for ACK from slave on port {slave_port}')
-                except ValueError:
-                    logger.error(f"Wrong ACK received from slave on port {slave_port}")
-                else:
-                    duration = time() - sent_time
-                    logger.debug(f'Wrote data {data} to slave ip with port {slave_port}, duration - {duration}')
+            concerns -= len(done)
+            if concerns <= 0:
+                break
             else:
-                logger.info(f"Not waiting for concern from slave on port {slave_port}")
+                logger.debug(f'waiting for {concerns} acks')
+    else:
+        await asyncio.sleep(2) # wait for tasks coroutines to start
 
     logger.debug(f'Whole function lasted for {time() - ts}')
     return "OK"
--- slave.py
+++ slave.py
@@ -2,23 +2,17 @@ import sys
 from threading import Thread
 
 from loguru import logger
-# from flask import Flask, jsonify
+import numpy as np
 from sanic import Sanic, json
 import socketio
-# from flask_socketio import SocketIO
 
 from args import parse_args
 
 cfg = parse_args()
 
-# sio = socketio.Server(async_mode='threading')
 sio = socketio.AsyncServer(async_mode='sanic', cors_allowed_origins=[])
-# app = socketio.ASGIApp(sio)
 app = Sanic(f"slave_{cfg['slave_id']}_app")
 sio.attach(app)
-# app = Flask(__name__)
-# app.wsgi_app = socketio.ASGIApp(sio, app.wsgi_app)
-# socketio = SocketIO(app, async_handlers=False)
 msg_lst = []
 
 
@@ -29,9 +23,14 @@ async def get_lst(_):
 
 @sio.on('append_msg')
 async def append_msg_handler(sid, data):
+    logger.debug(f'Received message {data} sid is {sid}')
+
+    wait_time = cfg["sleep_duration_sec"] + np.random.choice([-1, 1])
+    logger.debug(f'Waiting for {wait_time}')
+    await sio.sleep(wait_time)
+
     msg_lst.append(data)
-    logger.debug(f'Received message {data}')
-    await sio.sleep(cfg["sleep_duration_sec"])
+    logger.debug(f"Appended message {data}")
     await sio.emit('appended')
 
 
@@ -47,8 +46,7 @@ def main():
         logger.error(f"No such slave index {cfg['slave_id']}, in {cfg['slaves_ips']}")
         return 1
 
-    # socketio.run(app, host=slave_ip, port=slave_port, debug=True)
-    app.run(host=slave_ip, port=int(slave_port), debug=True)
+    app.run(host=slave_ip, port=int(slave_port))
 
 
 
